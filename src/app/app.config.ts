b72301e11bf4ad6f55b13af5a68f3f65a7dfd1a7
raulranga <raul.ranga@spyhce.com>
2024-02-07 20:45:57 +0200

    On dev service worker will not work, but using the script npm run start:prod which will run a build for production then it will run a server for testing production build locally

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 
+import {environment} from "../environments/environment.dev";
+
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
+        enabled: environment.production,
         registrationStrategy: 'registerWhenStable:30000'
     })]
 };
