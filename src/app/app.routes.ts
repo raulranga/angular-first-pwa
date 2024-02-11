e7d6e765740944c320a24c38aa779c243f6bdef9
raulranga <raul.ranga@spyhce.com>
2024-02-11 19:09:32 +0200

    Added caching policy for 2 api's in ngsw-config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 
 import { UsersComponent } from "./components/users/users.component";
 import { BreweriesComponent } from "./components/breweries/breweries.component";
+import {AppComponent} from "./app.component";
 
 export const routes: Routes = [
-  { path: 'users', component: UsersComponent },
-  { path: 'breweries', component: BreweriesComponent },
-
+  {
+    path: '',
+    children: [
+      { path: 'users', component: UsersComponent },
+      { path: 'breweries', component: BreweriesComponent },
+    ]
+  },
+  { path: '**', component: AppComponent }
 ];
